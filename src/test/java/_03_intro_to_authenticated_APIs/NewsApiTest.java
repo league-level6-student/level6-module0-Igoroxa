441b44e3ed48423fb16c4abed30641862c4fd895
league <league@imac-18.attlocal.net>
2023-10-19 18:27:31 -0700

    thursday finished

--- src/test/java/_03_intro_to_authenticated_APIs/NewsApiTest.java
+++ src/test/java/_03_intro_to_authenticated_APIs/NewsApiTest.java
@@ -26,35 +26,58 @@ import static org.mockito.Mockito.*;
 
 class NewsApiTest {
 
-    NewsApi newsApi;
-    
-    @Mock
-    NewsApi news;
+
+ 
+  NewsApi news;
+  
+  @Mock
+  Article art;
+  
+  @Mock
+  ApiExampleWrapper wrap;
 
     @BeforeEach
     void setUp() {
     	MockitoAnnotations.openMocks(this);
-    	
+    	news = new NewsApi();
     }
 
     @Test
     void itShouldGetNewsStoryByTopic() {
         //given
-    	boolean check = false;
-    	ApiExampleWrapper wrap = news.getNewsStoryByTopic("world");
-    	assertTrue(wrap.getTotalResults() != 0);
+    	ApiExampleWrapper example = news.getNewsStoryByTopic("world");
+    	List<Article> art = example.getArticles();
+    	String res = art.get(0).getTitle();
+    	
         //when
     	
         //then
+    	assertTrue(res.equals("Use 'Project-Based Learning' to Study on Your Own"));
+ 
     }
 
     @Test
     void itShouldFindStory(){
         //given
+    	ApiExampleWrapper wrap = new ApiExampleWrapper();
+    	List<Article> arts = null;
+    	wrap.setArticles(arts);
+    	arts.add(new Article());
+    	arts.get(0).setTitle("Use 'Project-Based Learning' to Study on Your Own");
+    	arts.get(0).setUrl("https://lifehacker.com/best-new-features-android-14-1850897684");
+    	arts.get(0).setContent("Made by Google 2023 is officially here, and with it, exciting product announcements. Weve now gotten our eyes on the Pixel 8 and Pixel 8 Pro, the Pixel Watch 2, and two new colors for Pixel Buds Pro.… [+3783 chars]");
+    	when(news.getNewsStoryByTopic("world")).thenReturn(wrap);
+
+        String test =
+                arts.get(0).getTitle() + " -\n"
+                        + arts.get(0).getContent()
+                        + "\nFull article: " + arts.get(0).getUrl();
 
         //when
-
-        //then
+    	String real = news.findStory("world");
+        //thenReturn
+    	
+    	assertTrue(real.equals(test));
     }
 
 
